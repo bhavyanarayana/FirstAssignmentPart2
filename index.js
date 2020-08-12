8bbb161ca5173dcc0a0c523a734f1323341ef223
bhavyanarayana <hn.bhavyagowda@gmail.com>
2020-08-12 19:33:00 +0530

    FirstAssignment part2

---
+++ index.js
@@ -0,0 +1,19 @@
+var express =  require('express');
+var fs = require('fs');
+var app = express();
+const port = process.env.port || 8080;
+app.listen(port, function(err, data){
+    console.log('Application is running on port: '+port);
+});
+
+app.get('/', function(req,res){
+    var data = fs.readFileSync('package.json');
+    console.log(data.toString());
+    //we can use like this
+    //res.setHeader('Content-Type', 'application/json');
+    //res.send(data.toString());
+
+    res.json(JSON.parse(data.toString()));
+
+  
+});
